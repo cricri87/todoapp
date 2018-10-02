b2e615b6266a7d00a0929c15bbe2b1cc61250b5f
Christian Rognstad <gudim87@hotmail.com>
2018-10-02 11:21:42 +0200

    Added edit action and index show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,9 @@
 class TodosController < ApplicationController
 
+  def index
+    @todos = Todo.all
+  end
+
   def new
     @todo = Todo.new
   end
@@ -16,6 +20,23 @@ class TodosController < ApplicationController
 
   def show
     @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was succsessfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
   end
 
   private
